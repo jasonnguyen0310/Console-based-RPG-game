e82f1a378769f26a27f050c053881cc32e3c2337
jasonnguyen0310 <jasonnguyen0310@gmail.com>
2021-04-15 02:50:52 -0700

    Copy and pasted from State.hpp

--- State.cpp
+++ State.cpp
@@ -1,1 +1,23 @@
+/*
+State.cpp
+Author: Jason Nguyen
+Date: April 14 2021
+*/
 #include "State.hpp"
+
+// This is a variadic template: it takes an arbitrary number of arguments...
+template <typename T, typename... Args> T State::*add_item(Args... args)
+{
+    // ... which we can pass to make_unique (which is also variadic) here.
+    // make_unique creates a unique_ptr and by calling the constructor with args...
+    auto item = std::make_unique<T>(args...);
+    auto ptr = item.get();
+    // Store the item in the vector so they are automatically deallocated when state is deallocated.
+    items.push_back(move(item));
+    return ptr;
+}
+
+void State::add_encounter(const Encounter &encounter)
+{
+    encounters.push(encounter);
+}
